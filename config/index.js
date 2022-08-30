aa04ac2c007d1f9214cd745b5fb6f257cd7d8486
hailso <112487462+hailso@users.noreply.github.com>
2022-08-30 23:18:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     // 模板消息id
     TEMPLATE_ID: "G8ijMPZKdhMWVRnYOg05Ib6fhysLyAGebuK_gRpSSF0",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: ["or-gf5olAXjrE7xcUZ1p6VQVvNBg"],
+    USERS: ["or-gf5olAXjrE7xcUZ1p6VQVvNBg","or-gf5oKYRRDvzOX-VJXqmRx-k9I"],
      
     /**
      * 信息配置
